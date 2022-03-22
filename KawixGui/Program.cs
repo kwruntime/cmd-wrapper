c4599694c5621dd2bd1b8a1e000cc1f296767ed2
James S <xtreme.maicolxt94@gmail.com>
2022-03-22 16:00:54 -0500

    Initial commit

---
+++ KawixGui/Program.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace KawixGui;
+
+internal class Program
+{
+	public static int Main(string[] args)
+	{
+		string location = Assembly.GetExecutingAssembly().Location;
+		int num = location.LastIndexOf("-gui.exe");
+		string text = "";
+		bool flag = true;
+		if (num >= 0)
+		{
+			text = location.Substring(0, num);
+		}
+		else
+		{
+			flag = false;
+			text = location.Substring(0, location.Length - 4);
+		}
+		Environment.SetEnvironmentVariable("GUI_MODE", "1");
+		Environment.SetEnvironmentVariable("FORCE_COLOR", "2");
+		string text2 = text + ".exe";
+		if (!flag || !File.Exists(text2))
+		{
+			text2 = text + ".bat";
+			if (!File.Exists(text2))
+			{
+				text2 = text + ".cmd";
+			}
+		}
+		Process process = new Process();
+		process.StartInfo.FileName = text2;
+		StringBuilder stringBuilder = new StringBuilder();
+		foreach (string text3 in args)
+		{
+			stringBuilder.Append("\"" + text3 + "\" ");
+		}
+		if (stringBuilder.Length > 0)
+		{
+			process.StartInfo.Arguments = stringBuilder.ToString();
+		}
+		process.StartInfo.UseShellExecute = false;
+		if (flag)
+		{
+			process.StartInfo.CreateNoWindow = true;
+		}
+		process.EnableRaisingEvents = true;
+		process.Start();
+		process.WaitForExit();
+		return process.ExitCode;
+	}
+}
